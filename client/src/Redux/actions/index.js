e0ad26582e130449b920e77bbf6c1d68469d7826
AngieVaMo <anvamopa2@gmail.com>
2022-07-06 08:01:24 -0500

    Fronted-2

---
+++ client/src/Redux/actions/index.js
@@ -0,0 +1,137 @@
+import axios from 'axios';
+export const GET_COUNTRY = 'GET_COUNTRY';
+export const GET_COUNTRY_BY_NAME = 'GET_COUNTRY_BY_NAME';
+export const GET_DETAILS = 'GET_DETAILS;'
+export const ORDER_BY_COUNTRY = 'ORDER_BY_COUNTRY';
+export const FILTER_BY_CONTINENT = 'FILTER_BY_CONTINENT';
+export const FILTER_BY_POPULATION = 'FILTER_BY_POPULATION';
+export const GET_ONLY_COUNTRIES = 'GET_ONLY_COUNTRIES';
+export const GET_ACTIVITY = 'GET_ACTIVITY';
+export const GET_COUNTRY_BY_ACTIVITY = 'GET_COUNTRY_BY_ACTIVITY';
+
+export function getCountry(){
+    return async function(dispatch) {
+        try {
+            let country = await axios.get("http://localhost:3001/api/Country");
+
+        return dispatch({
+            type: "GET_COUNTRY",
+            payload: country.data
+        })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
+
+export function getCountryByName(name){
+    return async function(dispatch) {
+        try {
+            let countryByName = await axios.get(`http://localhost:3001/api/Country?name=${name}`);
+
+            return dispatch({
+              type: "GET_COUNTRY_BY_NAME",
+              payload: countryByName.data
+            })
+
+        } catch (error) {
+            console.log(error)
+            alert("Country not found")
+        }
+    }
+}
+
+export function getDetails(id) {
+    return async function(dispatch) {
+        try {
+            let details = await axios.get(`http://localhost:3001/api/Country/${id}`)
+            
+            return dispatch({
+                type: "GET_DETAILS",
+                payload: details.data
+            })
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
+
+export function orderByCountry(payload){
+    return{
+        type: "ORDER_BY_COUNTRY",
+        payload
+    }
+}
+
+export function filterByContinent(payload){
+    return {
+      type: "FILTER_BY_CONTINENT",
+      payload
+    }
+  }
+  
+  export function filterByPopulation(payload){
+    return {
+      type: "FILTER_BY_POPULATION",
+      payload
+    }
+  }
+
+  export function getOnlyCountries(){
+    return function(dispatch) {
+        try {
+            let onlyCountries = await axios.get("http://localhost:3001/api/AllCountries").data
+
+            return dispatch({
+                type: "GET_ONLY_COUNTRIES",
+                payload: onlyCountries
+            })
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+  }
+
+  export function postActivity(payload){
+    return async function(dispatch){
+        try {
+            let activity = await axios.post("http://localhost:3001/api/TourActivity", payload).data
+
+            return dispatch({
+                type: "GET_ACTIVITY",
+                payload: activity
+            })
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+  }
+
+  export function getActivity(){
+    return function(dispatch){
+        try {
+            let activity = await axios.get("http://localhost:3001/api/TourActivity").data;
+
+            return dispatch({
+                type: "GET_ACTIVITY",
+                payload: activity
+            })
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+  }
+
+  export function countryByActivity(payload){
+    console.log("pay " + payload);
+    return {
+        type: "COUNTRY_BY_ACTIVITY",
+        payload
+    }
+  }
+
+
---
+++ client/src/Redux/reducer/index.js
@@ -0,0 +1,126 @@
+import {
+    GET_COUNTRY,
+  GET_COUNTRY_BY_NAME,
+  GET_DETAILS,
+  FILTER_BY_CONTINENT,
+  FILTER_BY_POPULATION,
+  ORDER_BY_COUNTRY,
+  GET_ONLY_COUNTRIES,
+  GET_TOUR_ACTIVITY,
+  COUNTRY_BY_ACTIVITY, 
+  GET_ACTIVITY
+} from "../actions/index.js";
+
+const initialState = {
+    country: [],
+    allCountries: [],
+    details: {},
+    onlyCountries: [],
+    activity: []
+}
+
+function reducer(state = initialState, action){
+    switch(action.type){
+        case GET_COUNTRY:
+            return{
+                ...state,
+                country: action.payload,
+                allCountries: action.payload
+            };
+        
+        case GET_COUNTRY_BY_NAME:
+            return{
+                ...state,
+                country: action.payload
+            };
+
+        case GET_DETAILS:
+            return{
+                ...state,
+                details: action.payload
+            };
+
+        case ORDER_BY_COUNTRY:
+            let countriesSorted = action.payload === 'asc' ?
+            state.country.sort(function (a,b) {
+              if (a.nameCountry > b.nameCountry){
+                return 1;
+              }
+              if (b.nameCountry > a.nameCountry){
+                return -1;
+              }
+              return 0;
+            }) :
+            state.country.sort(function (a,b) {
+              if (a.nameCountry > b.nameCountry){
+                return -1;
+              }
+              if (b.nameCountry > a.nameCountry){
+                return 1;
+              }
+              return 0;
+            })
+            return {
+              ...state,
+              country: countriesSorted
+            }
+
+        case FILTER_BY_CONTINENT:
+           let CountriesByContinent = state.allCountries;
+           let filteredByContinent = action.payload === "All" ? CountriesByContinent : CountriesByContinent.filter(c => c.continent === action.payload);
+           return{
+            ...state,
+            country: filteredByContinent
+           }
+
+        case FILTER_BY_POPULATION:
+          let sortedByPopulation = action.payload === "Low to High" ?
+          state.country.sort((a,b) => {
+            if(a.population > b.population){
+              return 1
+            }
+            if(a.population < b.population){
+              return -1
+            }
+            return 0
+          }) :
+          state.country.sort((a,b) => {
+            if(a.population > b.population){
+              return -1
+            }
+            if(a.population < b.population){
+              return 1
+            }
+            return 0
+          })
+          return{
+            ...state,
+            country: sortedByPopulation
+          }
+        
+        case GET_ONLY_COUNTRIES:
+          return{
+            ...state,
+            onlyCountries: action.payload
+          }
+        
+        case GET_ACTIVITY:
+          return{
+            ...state,
+            activity: action.payload
+          }
+
+        case COUNTRY_BY_ACTIVITY:
+          let country = state.country;
+          let filteredByActivity = action.payload === "All" ? country : country.filter(c => c.activity.find(a => a.nameActivity === action.payload));
+          return{
+            ...state,
+            country: filteredByActivity
+          }
+
+        default:
+          return state;
+    }
+}
+
+export default reducer;
